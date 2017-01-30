cbdf7ea0559b59062cc1e633e5acf2d8d229e0e6
Jenny Chan <jrc337@cornell.edu>
2017-01-30 17:50:14 -0500

    made user recordings and admin reducers

--- src/native/reducer/index.native.js
+++ src/native/reducer/index.native.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import currentUserReducer from './UserReducer.native';
+import userRecordingsReducer from './users.native';
+import adminReducer from './admin.native';
 
 const rootReducer = combineReducers({
-  currentUser: currentUserReducer
+  currentUser: currentUserReducer,
+  userRecordings: userRecordingsReducer,
+  admin: adminReducer
 });
 
 export default rootReducer;
