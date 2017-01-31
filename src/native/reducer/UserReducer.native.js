83ee2fcb677bc1c32be47d2f9f7a441c9eca95e2
Jimmy Wang <jawang12@gmail.com>
2017-01-31 11:09:29 -0500

    updated reducer

--- src/native/reducer/UserReducer.native.js
+++ src/native/reducer/UserReducer.native.js
@@ -27,28 +27,7 @@ export const currentUser = (authenticatedUser) => {
 
 /* -----------------    DISPATCHERS     ------------------ */
 
-// export const verifyLogin = (username, password) => dispatch => {
-//   axios.get('watson-backend.herokuapp.com/api/watson')
-// };
 
-// export const registerUser = (credentials) => dispatch => {
-//   axios.post('http://watson-backend.herokuapp.com/api/users/', credentials)
-//   .then(response => {
-//     dispatch(newUser(response.data));
-//   })
-//   .catch(err => console.error('failed to post', err));
-// };
-//
-
-// export const registerUser = (credentials) => dispatch => {
-//   axios.post('http://localhost/api/tokens/signup', credentials)
-//   .then(response => {
-//     dispatch(newUser(response.data.user));
-//     Actions.homepage();
-//     console.log('signed up!', response.data.user)
-//   })
-//   .catch(err => console.error('failed to post', err));
-// };
 
 
 /* -----------------    REDUCER     ------------------ */
