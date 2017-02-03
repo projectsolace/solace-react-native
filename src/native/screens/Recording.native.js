4d45491d20b9d03dd9b80241ab57bef2b9fdcf48
Anuj Shah <ashah812@gmail.com>
2017-02-03 15:17:19 -0500

    recording component

--- src/native/screens/Recording.native.js
+++ src/native/screens/Recording.native.js
@@ -8,6 +8,8 @@ import { RNS3 } from 'react-native-aws3';
 import axios from 'axios'
 import secrets from './secrets.json';
 import {connect} from 'react-redux'
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
+
 
 
 let audioPath = AudioUtils.DocumentDirectoryPath + '/watson2.wav';
@@ -22,24 +24,44 @@ console.log('where are audioPath', audioPath)
 
 class Recording extends Component {
 
+    // Stop Watch
+   // <View style={styles.timer}>
+   //              <Stopwatch options={optionsA} start={this.state.stopwatchStart} reset={this.state.stopwatchReset}/>
+   //          </View>
+
     constructor(props, context) {
         super(props, context);
         this.state={
-          recording:false
+          recording:false,
+          stopwatchStart: false,
+          totalDuration: 90000,
+          stopwatchReset: false,
         }
 
+    this.toggleStopwatch = this.toggleStopwatch.bind(this);
+    this.resetStopwatch = this.resetStopwatch.bind(this);
+
     }
 
-    componentDidMount(){
+
+    toggleStopwatch() {
+      this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
     }
+
+    resetStopwatch() {
+      this.setState({stopwatchStart: false, stopwatchReset: true});
+    }
+
 
   render() {
+
 
 
     const onStartRecord = () => {
       console.log('STARTED RECORDING')
       AudioRecorder.startRecording();
       this.setState({recording:true})
+      this.toggleStopwatch()
 
     };
 
@@ -94,6 +116,8 @@ class Recording extends Component {
           })
       })
       .catch(err => console.log(err));
+      this.resetStopwatch()
+      this.toggleStopwatch()
       this.setState({recording:false})
 
     };
@@ -101,13 +125,30 @@ class Recording extends Component {
     return (
       <View>
           <Image source={require('./solace.png')} style={styles.image}/>
-          <Text style={styles.text}> {`"${this.props.quote.quote}"`}</Text>
+          <Text style={styles.text2}> {`Hello ${this.props.user.firstName},\n What's on your mind today?`}</Text>
           {!this.state.recording ? recordingMic(): stopMic()}
+          <Text style={styles.text}> {`"${this.props.quote.quote}"`}</Text>
           <Button info style={{alignSelf: 'center'}} onPress={()=> Actions.questionModal()} > Helpful Questions </Button>
       </View>
     );
   }
 }
+
+const handleTimerComplete = () => alert("custom completion function");
+
+const optionsA = {
+  container: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    padding: 20,
+    borderRadius: 5,
+    width: 175,
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -145,6 +186,20 @@ const styles = StyleSheet.create({
      height: 3
    },
    shadowRadius: 5,
+   shadowOpacity: 1.0,
+   marginBottom: 10
+  },
+  text2: {
+    alignSelf: 'center',
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    shadowColor: '#000000',
+   shadowOffset: {
+     width: 0,
+     height: 3
+   },
+   shadowRadius: 5,
    shadowOpacity: 1.0
   },
   phantom: {
@@ -152,6 +207,9 @@ const styles = StyleSheet.create({
   },
   phantom2: {
     height: 124
+  },
+  timer: {
+    alignSelf: 'center',
   }
 
 });
