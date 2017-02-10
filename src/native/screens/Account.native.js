5d8f4a186c63ca6636cb1c39388a52555bc99b84
mobiuschang <mobiuschang@gmail.com>
2017-02-09 19:04:31 -0500

    Refactored Account detail options list

--- src/native/screens/Account.native.js
+++ src/native/screens/Account.native.js
@@ -6,22 +6,9 @@ import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { updateCurrentUser, logoutUser } from '../reducer/user.native';
 import { BlurView } from 'react-native-blur';
+import {occupation, income, ethnicity, religion, education, maritalStatus } from '../dataList';
 
 const Item = Picker.Item;
-
-
-let occupation = ['Select', 'Sales', 'Hospitality', 'Healthcare', 'Custodial', 'Accounting', 'Teaching', 'Law-Enforcement', 'Law', 'Finance', 'Engineering', 'Administration', 'Student', 'Other'];
-
-let income = ['Select', 'Under-$15,000', '$15,000-to-$24,999', '$25,000-to-$34,999', '$35,000-to-$49,999', '$50,000-to-$74,999', '$75,000-to-$99,999', '$100,000-to-$149,999', '$150,000-to-$199,999', '$200,000-and-over'];
-
-let ethnicity = ['Select', 'White', 'Black', 'Hispanic', 'Asian', 'American-Indian/Alaska-Native', 'Hawaiian/Other-Pacific-Islander', 'Other'];
-
-let religion = ['Select', 'Protestant', 'Catholic', 'Mormon', 'Judaism', 'Islam', 'Buddhism', 'Hinduism', 'Other', 'Unaffiliated'];
-
-let education = ['Select', 'High-School', 'Some-College', 'Associate-Degree', 'Bachelor-Degree', 'Advanced-Degree'];
-
-let maritalStatus = ['Select', 'Single', 'Married', 'Widowed', 'Divorced'];
-
 
 class Account extends Component {
   constructor(props) {
@@ -63,6 +50,8 @@ class Account extends Component {
   }
 
   render() {
+
+    console.log('here is occupation', occupation);
     const { logoutUser } = this.props;
 
     let occupationList = occupation.map((job, i) => {
